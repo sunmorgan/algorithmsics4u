ee409cf30df8dfd58a6eeb842bef9eb3b5feb5b8
Jonathan Hung <83654311+HeidenCake@users.noreply.github.com>
2022-02-15 20:35:32 -0500

    Update SearchMethods.java

--- SearchMethods.java
+++ SearchMethods.java
@@ -1,10 +1,30 @@
 package algorithm;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class SearchMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		
 
+	}
+	
+	public static int exponentialSearch(int [] data, int searchNum) {
+		if(data[0] == searchNum) {
+			return 0;
+		}
+		int i = 1; 
+		while(i<data.length && data[i]<= searchNum) {
+			i *= 2;
+		}
+		data = Arrays.copyOfRange(data,i/2,Math.min(i,data.length));
+		int index = binarySearch(i/2,i,searchNum);
+		if(index >= 0) {
+			index += i/2;
+		}
+		return index; 
 	}
 
 }
+
