a4dc118a854e853be9707332902d71f15a4b1d72
Jonathan Hung <83654311+HeidenCake@users.noreply.github.com>
2022-02-16 22:33:44 -0500

    Update SortMethods.java

--- SortMethods.java
+++ SortMethods.java
@@ -3,12 +3,6 @@ package algorithm;
 public class SortMethods {
 
 	public static void main(String[] args) {
-		int [] arr = new int[] {4, 1, 18, 3, 5, 90, 7, 2, 9};
-		selectionSort(arr);
-		for(int x : arr) {
-			System.out.print(x + " ");
-			
-		}
 	}
 	
 	public static void insertionSort(int [] data) {
@@ -37,45 +31,4 @@ public class SortMethods {
 		data [x] = data[y];
 		data[y] = temp;
 	}
-	public static void bubbleSort(int [] data) {
-		//if everything is ordered, break the whole loop
-		//compare with the one to the right 
-		        //if it's greater, swap
-		//the maximum times it will sort through each algorithm will be the length of it -1 since 
-		//if you swap from the very right to the very left it will go through it data.length-1 times until it hit 0
-		for(int x= data.length-1;x>=0; x--) {
-			//make swapped false each time the whole sorting begins again 
-			boolean swapped = false; 
-			
-			for(int y = 0; y<x; y++) {
-				if(data[y]>data[y+1]) {
-					swap(data,y,y+1);
-					//showing that something was actually swapped 
-					swapped = true; 
-				}
-			}
-			//meaning that nothing needs to be swapped anymore 
-			if(!swapped) {
-				break;
-			}
-		}
-	}
-	public static void selectionSort(int [] data) {
-		//set the first index of the array as current minimum 
-		//checks over the whole array until you find the number lower (if you find two that are lower, set one as minimum 
-		// but then changes when you see something lower
-		//swap it with the current minimum number 
-		//move the current minimum number to the next index
-		for(int x = 0; x<data.length-1; x++) {
-			int smallest = x; 
-			for(int y = x+1; y<data.length; y++) {
-				if(data[y]<data[smallest]) {
-					smallest = y; 
-				}
-			}
-			swap(data,x,smallest);	
-		}
-	}
-
-
 }
