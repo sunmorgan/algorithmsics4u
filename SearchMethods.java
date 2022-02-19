11993953e8d9ef62ecadc2938fcf68934fda8478
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2022-02-19 11:55:59 -0500

    Update SearchMethods.java

--- SearchMethods.java
+++ SearchMethods.java
@@ -36,6 +36,29 @@ public class SearchMethods {
 		}
 		return index; 
 	}
+	
+	public void sort(int array[])
+    {
+        for (int i = 1; i < array.length; i++)
+        {
+            int x = array[i];
+ 
+            // Find location to insert
+            // using binary search
+            int j = Math.abs(
+                Arrays.binarySearch(array, 0,
+                                    i, x) + 1);
+ 
+            // Shifting array to one
+            // location right
+            System.arraycopy(array, j,
+                             array, j + 1, i - j);
+ 
+            // Placing element at its
+            // correct location
+            array[j] = x;
+        }
+    }
 
 }
 
